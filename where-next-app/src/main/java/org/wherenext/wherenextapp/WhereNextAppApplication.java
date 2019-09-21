11e41b271be892153643ea296be1d5c047852d57
Darren Hou <hou.darren@gmail.com>
2019-09-21 11:43:03 -0500

    Set up spring

---
+++ where-next-app/src/main/java/org/wherenext/wherenextapp/WhereNextAppApplication.java
@@ -0,0 +1,13 @@
+package org.wherenext.wherenextapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereNextAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereNextAppApplication.class, args);
+	}
+
+}
