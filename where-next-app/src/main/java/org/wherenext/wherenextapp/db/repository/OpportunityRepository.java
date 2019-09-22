cc077f5d59c028ab66dedc744cf8ad94f47fefb4
Darren Hou <hou.darren@gmail.com>
2019-09-21 23:52:52 -0500

    Change text cleaning algorithm

--- where-next-app/src/main/java/org/wherenext/wherenextapp/db/repository/OpportunityRepository.java
+++ where-next-app/src/main/java/org/wherenext/wherenextapp/db/repository/OpportunityRepository.java
@@ -3,5 +3,10 @@ package org.wherenext.wherenextapp.db.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.wherenext.wherenextapp.db.model.Opportunity;
 
+/**
+ * Repository for volunteer opportunities
+ *
+ * @author darren.hou
+ */
 public interface OpportunityRepository extends CrudRepository<Opportunity, Integer> {
 }
--- where-next-app/src/main/java/org/wherenext/wherenextapp/service/WherenextService.java
+++ where-next-app/src/main/java/org/wherenext/wherenextapp/service/WherenextService.java
@@ -1,6 +1,4 @@
 package org.wherenext.wherenextapp.service;
 
 public class WherenextService {
-
-
 }
